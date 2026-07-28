c22de2748e8fec352e96205daf1aee34b8b23b02
kauamalmeida-png <kauam.almeida@escola.pr.gov.br>
2026-07-28 11:34:56 -0300

    ..

--- perguntas.js
+++ perguntas.js
@@ -2,10 +2,169 @@
 // Troque disciplina, pergunta e resposta pelos conteúdos que você criou.
 
 criaCartao(
-   'Disciplina',
+   'Física',
+   'O que é força?',
+   'Força é uma interação capaz de alterar o movimento ou a forma de um corpo.'
+    
+   
+)
+   
+criaCartao(
+   'Física', 
+    'O que diz a Primeira Lei de Newton?',
+    'Resposta'
+)
+
+criaCartao(
+   'Arte', 
+    'O que caracteriza o Modernismo?',
+    'Resposta'
+)
+
+criaCartao(
+   'Arte', 
+    'O que é arte?',
+    'Resposta'
+)
+
+criaCartao(
+   'Física e Tecnociência', 
+    'O que é tecnociência?',
+    'Resposta'
+)
+
+criaCartao(
+   'Física e Tecnociência', 
+    'Como a Física contribui para a tecnologia?',
+    'Resposta'
+)
+
+criaCartao(
+   'Educação Física', 
+    'Quais são os benefícios da atividade física?',
+    'Resposta'
+)
+
+criaCartao(
+   'Educação Física', 
+    'O que é aquecimento físico?',
+    'Resposta'
+)
+
+criaCartao(
+   'Programação', 
+    'O que é um algoritmo?',
+    'Resposta'
+)
+
+criaCartao(
+   'Programação', 
+    '. O que é uma variável em programação?',
+    'Resposta'
+)
+
+criaCartao(
+   'História', 
+    'O que foi a Revolução Industrial?',
+    'Resposta'
+)
+
+criaCartao(
+   'História', 
+    'O que foi a Revolução Francesa?',
+    'Resposta'
+)
+
+criaCartao(
+   'Robótica', 
+    'O que é robótica?',
+    'Resposta'
+)
+
+criaCartao(
+   'Robótica', 
+    'Quais são os principais componentes de um robô?',
+    'Resposta'
+)
+
+criaCartao(
+   'Educação Financeira', 
+    'O que é orçamento financeiro?',
+    'Resposta'
+)
+
+criaCartao(
+   'Educação Financeira', 
+    'Qual é a diferença entre poupar e investir?',
+    'Resposta'
+)
+
+criaCartao(
+   'Sociologia', 
+    'O que é cultura?',
+    'Resposta'
+)
+
+criaCartao(
+   'Sociologia', 
+    'O que é cidadania?',
+    'Resposta'
+)
+
+criaCartao(
+   'Filosofia', 
+    'O que é ética?',
+    'Resposta'
+)
+
+criaCartao(
+   'Filosofia', 
+    'Quem foi Sócrates?',
+    'Resposta'
+)
+
+criaCartao(
+   'Língua Inglesa', 
+    'Quando usamos o Simple Present?',
+    'Resposta'
+)
+
+criaCartao(
+   'Língua Inglesa', 
+    'Qual é a diferença entre "there is" e "there are"?',
+    'Resposta'
+)
+
+criaCartao(
+   'Língua Portuguesa', 
+    'O que é um substantivo?',
+    'Resposta'
+)
+criaCartao(
+   'Língua Portuguesa', 
+    'O que é um verbo?',
+    'Resposta'
+)
+criaCartao(
+   'matemática',
+   'O que é uma função exponencial e onde ela é aplicada?'
+    'Resposta'
+)
+
+criaCartao(
+   'matemática', 
+    '',
+    'Resposta'
+)
+
+criaCartao(
+   'matematicá', 
     'Pergunta',
     'Resposta'
 )
+
+
+
 
 
 
